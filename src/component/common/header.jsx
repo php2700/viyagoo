c8699969336ac9b89c7480898925847aa9457a42
dppatel8085 <devrajpatel8085@gmail.com>
2025-11-29 16:15:11 +0530

    logochange

--- src/component/common/header.jsx
+++ src/component/common/header.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom"; 
-import logo from "../../assets/logo.png";
+import logo from "../../assets/logo1.png";
 import { Menu, X, ChevronDown } from "lucide-react";
 
 export const Header = () => {
